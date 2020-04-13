06a978ac28ff744ebe30390fc0b280ff5a3a0592
Serghei <seryga_ms@mail.ru>
2020-04-13 15:56:47 +0300

    Phase finale add animation and validation code

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,4 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1100,
-//         adaptiveHeight: false,
-//         prevArrow : '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-//         nextArrow : '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-            
-//             }
-//         ]
-//       });
-// });
-
+$(document).ready(function(){
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -65,3 +47,90 @@ const slider = tns({
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
   
+  // Modal
+
+  $('[data-modal="consultation"]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста,введите своё имя",
+          minlength: jQuery.validator.format("Введите {0} символов!")
+        },
+        phone: "Пожалуйста,введите свой номер телефона",
+        email: {
+          required: "Пожалуйста,введите свою почту",
+          email: "Неправильно введен адресс почты name@domain.com"
+        }
+      }
+    });
+  } 
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  //Mask
+
+  $('input[name=phone]').mask("+373 (99) 99-99-99");
+
+  //PHP AJAX delivered form frontend parts
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val(""); //после отправки всех данных на сервер мы очищаем поля
+
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //Smooth scroll and page Up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });      
+    $("a[href=#up]").click(function(){
+      const_href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
+  new WOW().init();
+});
